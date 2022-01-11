eed4e473955a565c1f9eb6b26480c4712805af56
dmdhrumilmistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-01-11 15:06:37 +0530

    Update ubuntu.sh
    - fix bugs
    - add banner
    - echo useful information to user

--- Installation/linux_distros/ubuntu.sh
+++ Installation/linux_distros/ubuntu.sh
@@ -1,8 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\033[33m[*] Script downloaded from https://github.com/dmdhrumilmistry/TermuxScripts\033[0m"
+# banner
+echo "========================================================="
+echo -e "\033[33m                          ___                           \033[0m"
+echo -e "\033[33m | | |_      ._ _|_        |  ._   _ _|_  _. | |  _  ._ \033[0m"
+echo -e "\033[33m |_| |_) |_| | | |_ |_|   _|_ | | _>  |_ (_| | | (/_ |  \033[0m"
+echo -e "\033[33m                                                        \033[0m"
+echo "========================================================="
 echo -e "\033[33m---- Written by dmdhrumilmistry ----\033[0m"
+echo "========================================================="
+echo -e "\033[33m[*] Script downloaded from https://github.com/dmdhrumilmistry/TermuxScripts\033[0m"
+# end banner
 
+# start installation
 echo -e "\033[33m[*] Updating packages...\033[0m"
 pkg update -y
 
@@ -23,14 +33,20 @@ BASHRC_FILE=$HOME/.bashrc
 
 # Commands
 START_UBUNTU="start-ub"
+RM_UBUNTU="rm-ub"
 
+
+# delete alias file if already exists
+if [ -f "$ALIAS_FILE" ]; then 
+    rm -f $ALIAS_FILE
+fi
 touch $ALIAS_FILE
 echo "####################################" >> $ALIAS_FILE
 echo "### Aliases for UBUNTU in Termux ###" >> $ALIAS_FILE 
 echo "####################################" >> $ALIAS_FILE
 echo "" >> $ALIAS_FILE
-echo "# for startup" >> $ALIAS_FILE
-echo "alias $START_UBUNTU=proot-distro login ubuntu" >> $ALIAS_FILE
+echo "alias $START_UBUNTU=\"proot-distro login ubuntu\"" >> $ALIAS_FILE
+echo "alias $RM_UBUNTU=\"proot-distro remove ubuntu\"" >> $ALIAS_FILE
 echo "" >> $ALIAS_FILE
 
 # create .bashrc file if does not exist
@@ -40,5 +56,19 @@ fi
 echo "source $ALIAS_FILE" >> $BASHRC_FILE
 echo "" >> $BASHRC_FILE
 
-echo -e "\033[33m[*] Ubuntu has been installed...[0m"
-echo -e "\033[33m[*] Start Ubuntu using $START_UBUNTU\033[0m"
+# echos to user
+echo -e "\033[33m[*] Ubuntu has been installed...\033[0m"
+echo "========================================================="
+echo -e "\033[33m[*] To start Ubuntu use:\n$START_UBUNTU\033[0m\n"
+echo -e "\033[33m[*] To remove Ubuntu use:\n$RM_UBUNTU\033[0m\n"
+echo "========================================================="
+
+echo -e "\033[33m[*] if you face any issues, do consider creating a issue on GitHub: https://github.com/dmdhrumilmistry/TermuxScripts/issues\033[0m"
+echo -e "\033[33m[*] Running this script again, might fix issues in some cases.\033[0m"
+
+echo "========================================================="
+echo -e "\033[33m[*] Do Restart Termux before using commands.\033[0m"
+echo "========================================================="
+
+# load alias file
+source $ALIAS_FILE
